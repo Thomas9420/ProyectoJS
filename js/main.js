6736ef8e783b4451ef087b1bf645398dd3b89728
Thomas Polo <thomaspolo99@gmail.com>
2023-07-19 23:37:36 -0300

    Segunda preentrega

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
-function mayorNum(){
-    var num1 = parseInt(document.getElementById("num1").value);
-    var num2 = parseInt(document.getElementById("num2").value);
+let nombreUsuario = prompt("Hola! ¿Cómo te llamas?");
+console.log(nombreUsuario);
+alert("Bueno, " + nombreUsuario + ", si estás acá me imagino que te gusta la cerveza!");
 
-    if(num1 && num2 ){
-        if (num1 >=num2){
-        if (num1== num2){
-            alert("Los numeros son iguales")
-        }else{
-            alert("El primero es mayor")
-        }
-    }else{
-        alert("El segundo es mayor")
-    }
-    }else{
-        alert("Falta colocar un numero")
-    }
+function Producto(alcohol, color, tipo, precio) {
+  this.alcohol = alcohol;
+  this.color = color;
+  this.tipo = tipo;
+  this.precio = precio;
+}
+
+const producto1 = new Producto("Fuerte", "Negra", "Amarga", 700);
+const producto2 = new Producto("Medio", "Ambar", "Equilibrada", 650);
+const producto3 = new Producto("Tranquila", "Dorada", "Suave", 600);
+
+const cerveza = prompt("¿amarga, equilibrada o suave?");
+alert("Esta es la mas apta para vos! Que la disfrutes.");
+
+switch (cerveza) {
+  case "amarga":
+    console.log(producto1);
+    break;
+  case "equilibrada":
+    console.log(producto2);
+    break;
+  case "suave":
+    console.log(producto3);
+    break;
+  default:
+    console.log("No existe o no está de momento en nuestro catalogo:(, probá recargando la pagina e intenta de nuevo :D");
 }
